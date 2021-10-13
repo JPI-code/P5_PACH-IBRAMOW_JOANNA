9f8ee2741fdfed00bfe646e008cfe7d14be12ea2
Joanna Pach-Ibramow <joanna.pach.ibramow@gmail.com>
2021-10-13 10:53:17 +0200

    Product page - description and options

--- front/js/product.js
+++ front/js/product.js
@@ -6,8 +6,57 @@ const getProductId = () => {
    let id = url.slice (index, url.length);
    id = id.slice(4, url.length);
 
-   console.log (id)
+   return id
 }
 
 
 getProductId ()
+
+
+// http://localhost:3000/api/products
+
+// const itemListContainer = document.getElementById("items");
+// let fetchedItems = [];
+// const fetchItems = async () => {
+//     await fetch("http://localhost:3000/api/products")
+//     .then((response) => response.json())
+//     .then((data) => (fetchedItems = data))
+//         .catch(err => console.log(err))
+// };
+
+
+const itemProductContainer =document.getElementById("item");
+let fetchedProduct;
+const fetchProduct = async () => {
+   let ProductId = getProductId();
+   await fetch("http://localhost:3000/api/products/"+ProductId)
+   //in order to add string + text
+   .then((response) => response.json())
+   .then((data) => (fetchedProduct= data))
+   // for debug only
+   // .then(() => console.log(fetchedProduct))
+       .catch(err => console.log(err))
+}
+
+fetchProduct ()
+// console.log(fetchedProduct)
+
+const displayProduct = async () => {
+await fetchProduct()
+document.getElementById("title").innerText=fetchedProduct.name;
+// "name" comes from Product.js
+document.getElementById("price").innerText=fetchedProduct.price;
+document.getElementById("description").innerText=fetchedProduct.description;
+let selectedColorContainer = document.getElementById("colors");
+// "colors" comes from Product.js
+fetchedProduct.colors.forEach(color => {
+   // color - my variable, one element from array COLORS
+   let newOption = document.createElement("option")
+   // createElement is a methode
+   newOption.innerText=color;
+   newOption.value=color;
+   // <option value="vert">vert</option>
+selectedColorContainer.appendChild(newOption);
+});
+}
+displayProduct()
